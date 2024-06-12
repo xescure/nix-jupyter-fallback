0333d23b00001574644932505ec59bd2df4b6b06
xescure <xescure@xescu.re>
2024-06-12 22:27:29 +0200

    minimum to get started with data science

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A development shell for Python projects";
+
+  inputs = {
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/nixos-24.05"; # You can adjust the nixpkgs version as needed
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.poetry ] ++ [
+            (pkgs.python3.withPackages (ps:
+              with ps; [
+                ps.jupyter
+                ps.ipython
+
+                # ps.pandas
+                # ps.numpy
+                # ps.matplotlib
+                # ps.keras
+                # ps.tensorflow
+              ]))
+          ];
+        };
+      });
+}
