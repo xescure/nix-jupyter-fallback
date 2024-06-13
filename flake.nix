cfd6eed0dabeb7e8040f4df7d173a249274279f4
xescure <xescure@xescu.re>
2024-06-13 13:48:26 +0200

    idk at this point

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "A development shell for Python projects";
 
   inputs = {
-    nixpkgs.url =
-      "github:NixOS/nixpkgs/nixos-24.05"; # You can adjust the nixpkgs version as needed
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -11,20 +11,38 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ pkgs.poetry ] ++ [
-            (pkgs.python310.withPackages (ps:
-              with ps; [
-                ps.jupyter
-                ps.ipython
+        devShells.default = (pkgs.buildFHSUserEnv {
+          name = "fhspython";
+          targetPkgs = (pkgs: [
+            # pkgs.poetry
+            pkgs.python310
+            pkgs.python310Packages.pip
+            pkgs.python310Packages.virtualenv
+            # pkgs.python310Packages.jupyter
+            # pkgs.python310Packages.ipython
+            # pkgs.cudaPackages.cudatoolkit
+          ]);
+            # (with pkgs; [
+            #   poetry
+            #   python310
+            #   # cudaPackages.cudatoolkit
+            # ]);
+          runScript = "bash";
+        }).env;
+        # devShells.default = pkgs.mkShell {
+        #   buildInputs = [ pkgs.poetry ] ++ [
+        #     (pkgs.python310.withPackages (ps:
+        #       with ps; [
+        #         ps.jupyter
+        #         ps.ipython
 
-                # ps.pandas
-                # ps.numpy
-                # ps.matplotlib
-                # ps.keras
-                # ps.tensorflow
-              ]))
-          ];
-        };
+        #         # ps.pandas
+        #         # ps.numpy
+        #         # ps.matplotlib
+        #         # ps.keras
+        #         # ps.tensorflow
+        #       ]))
+        #   ];
+        # };
       });
 }
